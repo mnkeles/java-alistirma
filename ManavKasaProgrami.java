a7825146f96c91ccee6be3bd0b3dec3966d7f74e
Muhammed Necati Keles <95759870+mnkeles@users.noreply.github.com>
2021-12-26 18:20:44 +0300

    Create ManavKasaProgrami.java

---
+++ ManavKasaProgrami.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class manavKasa {
+    public static void main(String[]  args){
+        Scanner input= new Scanner(System.in);
+        double armut=2.14,elma=3.67, domates=1.11, muz=0.95 ,patlican=5.00,a,toplam,e,d,m,p;
+        System.out.println("Alınacakların kilogramını yazınız:");
+
+        System.out.print("Armut=");
+        a=input.nextDouble();
+
+        System.out.print("Elma=");
+        e=input.nextDouble();
+
+        System.out.print("Domates=");
+        d=input.nextDouble();
+
+        System.out.print("Muz=");
+        m=input.nextDouble();
+
+        System.out.print("Patlıcan=");
+        p=input.nextDouble();
+
+        toplam=((armut*a)+(elma*e)+(domates*d)+(muz*m)+(patlican*p));
+        System.out.print("Toplam="+ toplam);
+        
+    }
+
+}
